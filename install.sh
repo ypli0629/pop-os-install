b2afb77f5385816546493f6dfad4d9b153609fb2
ypli0629 <liyapeng0629@gmail.com>
2024-09-08 17:42:01 +0800

    feat: add flathub mirror

--- install.sh
+++ install.sh
@@ -6,26 +6,36 @@ do
     mkdir -p "~/Desktop/$path"
 done
 
-mkdir -p ~/Desktop/source
-mkdir -p ~/Desktop/work
-mkdir -p ~/Desktop/caffe
-mkdir -p ~/Desktop/learn
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo flatpak remote-modify flathub --url=https://mirror.sjtu.edu.cn/flathub
 
-sudo add-apt-repository ppa:graphics-drivers/ppa
-sudo apt update
-sudo apt install nvidia-driver-550 -y
-
-# apt
-sudo apt install wget curl git zsh build-essential fcitx5-chinese-addons gnome-browser-connector gnome-tweaks gnome-shell-extensions -y
+# sudo add-apt-repository ppa:graphics-drivers/ppa
+# sudo apt update
+# sudo apt install nvidia-driver-560 -y
 
 # snap
-packages=("clion" "cmake" "code" "datagrip" "gnome-calendar" "go" "goland" "intellij-idea-ultimate" "multipass" \
-    "postman" "pycharm-professional" "rustup" "telegram-desktop" "termius-app" "todoist" "typora" "webstorm" "xmind")
+packages=("com.calibre_ebook.calibre" \ 
+    "com.discordapp.Discord" \
+    "com.getpostman.Postman" \
+    "com.google.Chrome" \ 
+    "com.jetbrains.IntelliJ-IDEA-Ultimate" \
+    "com.obsproject.Studio" \
+    "com.qq.QQ" \
+    "com.qq.QQmusic" \
+    "com.tencent.WeChat" \
+    "com.visualstudio.code" \
+    "io.github.shiftey.Desktop" \
+    "org.blender.Blender" \
+    "org.gimp.GIMP" \ 
+    "org.telegram.desktop" \
+    "com.termius.Termius" \
+    "io.typora.Typora" \
+    "net.xmind.XMind")
 
 # Loop through each package and install it using snap
 for package in "${packages[@]}"
 do
-    sudo snap install "$package" --classic
+    sudo flatpak install "$package"
 done
 
 # docker-desktop
@@ -39,7 +49,7 @@ echo \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-wget https://desktop.docker.com/linux/main/amd64/docker-desktop-amd64.deb?utm_source=docker&utm_medium=webreferral&utm_campaign=docs-driven-download-linux-amd64
+wget https://desktop.docker.com/linux/main/amd64/docker-desktop-amd64.deb
 sudo apt install .//docker-desktop-amd64.deb -y
 
 # clash-rev
@@ -47,21 +57,10 @@ wget https://launchpadlibrarian.net/704701345/libjavascriptcoregtk-4.0-18_2.43.3
 wget https://launchpadlibrarian.net/704701349/libwebkit2gtk-4.0-37_2.43.3-1_amd64.deb
 sudo dpkg -i ./libjavascriptcoregtk-4.0-18_2.43.3-1_amd64.deb ./libwebkit2gtk-4.0-37_2.43.3-1_amd64.deb
 wget https://github.com/clash-verge-rev/clash-verge-rev/releases/download/v1.6.0/clash-verge_1.6.0_amd64.deb
-sudo apt install ./clash-verge_1.6.0_amd64.deb
-
-# grub theme
-wget https://raw.githubusercontent.com/AdisonCavani/distro-grub-themes/master/themes/ubuntu.tar
-sudo mkdir -p /boot/grub/themes/ubuntu
-sudo tar -xvf ./ubuntu.tar -C /boot/grub/themes/ubuntu
-sudo echo "GRUB_THEME=/boot/grub/themes/ubuntu/theme.txt" >> /etc/default/grub
-sudo update-grub
+sudo apt install ./clash-verge_1.7.7_amd64.deb
 
 # switchhosts
 wget https://github.com/oldj/SwitchHosts/releases/download/v4.2.0-beta/SwitchHosts_linux_amd64_4.2.0.6105.deb
 sudo apt install ./SwitchHosts_linux_amd64_4.2.0.6105.deb -y
-
-# chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb -y
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
