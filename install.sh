30bd245aedd57e2d0efb4f28598e11896e9cffa5
ypli0629 <liyapeng0629@gmail.com>
2024-09-24 21:00:45 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -72,5 +72,17 @@ sudo apt install ./SwitchHosts_linux_amd64_4.2.0.6105.deb -y
 
 # zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+tee -a ~/.zshrc <<-'EOF'
+	alias szsh="source ~/.zshrc"
+	alias nzsh="nvim ~/.zshrc"
+	alias pon="export http_proxy=http://127.0.0.1:7890; export https_proxy=http://127.0.0.1:7890; export all_proxy=socks5://127.0.0.1:7890"
+	alias poff="unset http_proxy; unset https_proxy; unset all_proxy"
+
+	export PATH=~/.local/bin:~/go/bin:~/.local/bin:$PATH
+	___MY_VMOPTIONS_SHELL_FILE="${HOME}/.jetbrains.vmoptions.sh"; if [ -f "${___MY_VMOPTIONS_SHELL_FILE}" ]; then . "${___MY_VMOPTIONS_SHELL_FILE}"; fi
+EOF
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+source ~/.zshrc
 # brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
