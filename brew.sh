4c2fd2d16c5ac895cea153991643faf7e8d2c146
ypli0629 <liyapeng0629@gmail.com>
2025-02-05 18:35:19 +0800

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -3,8 +3,7 @@
 # brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-echo >> /home/ypli/.zshrc 
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/ypli/.zshrc
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' | sudo tee /etc/profile.d/brew.sh > /dev/null
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 brew install gcc go rustup node pnpm yarn neovim bear
