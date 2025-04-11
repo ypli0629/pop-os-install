631c2c4f5cfbe2aa0ab4e1e664dd63dc4cdd5f46
ypli0629 <liyapeng0629@gmail.com>
2025-04-11 18:24:15 +0800

    Update fcitx.sh

--- fcitx.sh
+++ fcitx.sh
@@ -2,7 +2,7 @@
 sudo apt install fcitx5-chinese-addons -y
 
 sudo mv /etc/profile.d/pop-im-ibus.sh /etc/profile.d/pop-im-ibus.sh.bak
-sudo echo "export INPUT_METHOD=fcitx\nexport GTK_IM_MODULE=fcitx\nexport QT_IM_MODULE=fcitx\nexport XMODIFIERS=@im=fcitx" | sudo tee /etc/profile.d/fcitx5.sh > /dev/null
+sudo echo -e "export INPUT_METHOD=fcitx\nexport GTK_IM_MODULE=fcitx\nexport QT_IM_MODULE=fcitx\nexport XMODIFIERS=@im=fcitx" | sudo tee /etc/profile.d/fcitx5.sh > /dev/null
 sh /etc/profile.d/fcitx5.sh
 
 curl -sSL https://www.debuggerx.com/fcitx5_customizer/fcitx5_customizer.sh | bash -s -- recommend
