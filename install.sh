fd685e77660707ec63a4965fc86ac3045258042b
ypli0629 <liyapeng0629@gmail.com>
2025-04-11 18:20:38 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -37,8 +37,8 @@ sudo apt install .//docker-desktop-amd64.deb -y
 # wget https://launchpadlibrarian.net/704701345/libjavascriptcoregtk-4.0-18_2.43.3-1_amd64.deb
 # wget https://launchpadlibrarian.net/704701349/libwebkit2gtk-4.0-37_2.43.3-1_amd64.deb
 # sudo dpkg -i ./libjavascriptcoregtk-4.0-18_2.43.3-1_amd64.deb ./libwebkit2gtk-4.0-37_2.43.3-1_amd64.deb
-wget https://github.com/clash-verge-rev/clash-verge-rev/releases/download/v2.0.3/Clash.Verge_2.0.3_amd64.deb
-sudo apt install ./Clash.Verge_2.0.3_amd64.deb
+wget https://github.com/clash-verge-rev/clash-verge-rev/releases/download/v2.2.3/Clash.Verge_2.2.3_amd64.deb
+sudo apt install ./Clash.Verge_2.2.3_amd64.deb
 
 # switchhosts
 wget https://github.com/oldj/SwitchHosts/releases/download/v4.2.0-beta/SwitchHosts_linux_amd64_4.2.0.6105.deb
