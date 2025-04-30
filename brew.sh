447e62768d0388128ebe856064c95f1e9ab191e2
ypli0629 <liyapeng0629@gmail.com>
2025-04-30 13:50:37 +0800

    feat: optimize

--- brew.sh
+++ brew.sh
@@ -7,7 +7,7 @@ echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' | sudo tee /etc/pr
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 brew tap homebrew/linux-fonts
+brew install --cask font-sauce-code-pro-nerd-font
 ln -s /home/linuxbrew/.linuxbrew/share/fonts -t ~/.local/share
-brew install font-sauce-code-pro-nerd-font
 
 brew install gcc go rustup node pnpm yarn neovim bear
