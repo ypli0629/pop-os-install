2c68ab3485e731c9b8340c4d9ff21a78c7933182
ypli0629 <liyapeng0629@gmail.com>
2025-06-28 09:56:20 +0800

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -7,8 +7,6 @@
 echo 'PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"' | sudo tee /etc/environment.d/brew.conf > /dev/null
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-brew tap homebrew/linux-fonts
 brew install --cask font-sauce-code-pro-nerd-font
-ln -s /home/linuxbrew/.linuxbrew/share/fonts -t ~/.local/share
 
 brew install gcc go rustup node pnpm yarn neovim bear
